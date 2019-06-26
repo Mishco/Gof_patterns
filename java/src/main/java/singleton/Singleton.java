a3e44b47537391ed49504615f934c45184e26cda
Michal Slovik <michal.slovik@EXXETA.sk>
2019-06-26 10:47:54 +0200

    Add Simple Singleton Pattern in Java

---
+++ java/src/main/java/singleton/Singleton.java
@@ -0,0 +1,13 @@
+package singleton;
+
+class Singleton {
+    private static Singleton instance = new Singleton();
+
+    private Singleton() {
+        // empty private constructor
+    }
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
