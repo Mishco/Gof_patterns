111e86407d97fdac13805540322b89388e8c428a
Michal Slovik <michal.slovik@EXXETA.sk>
2019-06-26 08:56:18 +0200

    Initial commit

---
+++ python3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python3',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='misl',
+    author_email='',
+    description=''
+)
