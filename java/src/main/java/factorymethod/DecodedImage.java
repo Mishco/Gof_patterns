b877effceefab61aa0156f85036ed021153b82a1
Michal Slovik <michal.slovik@EXXETA.sk>
2019-07-02 14:53:41 +0200

    Add Factory Method design pattern in java

---
+++ java/src/main/java/factorymethod/DecodedImage.java
@@ -0,0 +1,16 @@
+package factorymethod;
+
+public class DecodedImage {
+    private String image;
+
+    DecodedImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "DecodedImage{" +
+                "image='" + image + '\'' +
+                '}';
+    }
+}
---
+++ java/src/main/java/factorymethod/FactoryMethod.java
@@ -0,0 +1,25 @@
+package factorymethod;
+
+class FactoryMethod {
+    private DecodedImage decodedImage;
+    private ImageReader reader;
+
+    FactoryMethod(String image) {
+        String format = image.substring(image.indexOf('.') + 1);
+        switch (format) {
+            case "gif":
+                reader = new GifReader(image);
+                break;
+            case "jpeg":
+                reader = new JpegReader(image);
+                break;
+            default:
+                throw new RuntimeException("Not supported format");
+        }
+        decodedImage = reader.getDecodedImage();
+    }
+
+    DecodedImage getDecodedImage() {
+        return decodedImage;
+    }
+}
